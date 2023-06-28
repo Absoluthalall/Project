f1318eef88e994f20cc0be5ec3bfdd198c76ce8d
Sacracia <tiger3551999@mail.ru>
2023-06-28 16:31:21 +0300

    commited

---
+++ CitiesSkylinesModmenu/Hacks.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace CitiesSkylinesModmenu
+{
+    internal class Hacks : MonoBehaviour
+    {
+        private bool _show = true;
+        Rect windowRect = new Rect(10f, 10f, 500f, 400f);
+
+        public void OnGUI()
+        {
+            if (_show)
+            {
+                windowRect = GUI.Window(0, windowRect, (int x) => GUI.DragWindow(), "Cheat Menu");
+            }
+        }
+
+        void ShowWindow(int windowId)
+        {
+            GUI.DragWindow();
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Insert))
+            {
+                _show = !_show;
+            }
+            else if (Input.GetKeyDown(KeyCode.Delete))
+            {
+                Loader.Unload();
+            }
+        }
+    }
+}
---
+++ CitiesSkylinesModmenu/Loader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace CitiesSkylinesModmenu
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<Hacks>();
+        }
+
+        public static void Unload()
+        {
+            _Unload();
+        }
+
+        private static void _Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+
+        private static GameObject Load;
+    }
+}
